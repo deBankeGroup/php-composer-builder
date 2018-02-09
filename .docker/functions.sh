f3cf6763dfb935c516131e4385c32acc4b572bbb
Dan Webb <dan.webb@damacus.io>
2018-02-09 11:54:47 +0000

    New functions (#8)
    * Update README.md
    
    * use 7.2 Alpine and the new functions
    
    * Allow users to overide ORG and script version

--- .docker/functions.sh
+++ .docker/functions.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 set +o pipefail
 
-export PROJECT="php-composer-builder"
-export DESCRIPTION="Docker container composer on PHP 7.1"
-export MAINTAINER="damacus"
-export FILE='Dockerfile'
+export DESCRIPTION="Docker container composer on PHP 7.2"
+if [[ -z $ORG ]];then 
+  ORG="damacus"
+fi
+if [[ -z $BUILD_SCRIPT_VER ]];then
+	BUILD_SCRIPT_VER='master'
+fi
 
 if ! [[ -e .docker/external_functions.sh ]];then
-  wget -O ".docker/external_functions.sh" "https://raw.githubusercontent.com/damacus/docker-build-scripts/master/functions.sh"
+  wget -O ".docker/external_functions.sh" "https://raw.githubusercontent.com/${ORG}/docker-build-scripts/${BUILD_SCRIPT_VER}/functions.sh"
 fi
 
 source ".docker/external_functions.sh"
